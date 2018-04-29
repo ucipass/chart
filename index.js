147270b0957b74e9106f6551b0d7e44f74607618
ucipass <ucipass@gmail.com>
2018-04-29 11:09:59 -0500

    change for interface and simulator

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var moment = require('moment')
 var express = require('express')
 const http = require('http');
 const url = require('url');
-var WebSocket = require('ws')
+const WebSocket = require('ws')
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-var Mychart = require('ucipass-chart')
+const Mychart = require('ucipass-chart')
 var chart = new Mychart("sensor")
-
 var msg = {
     json : {
         labels:[],
@@ -33,49 +32,65 @@ var msg = {
     }
 }
 
-wss.on('connection', function (ws,req) {
-    const location = url.parse(req.url, true);
-    ws.on('error', () => console.log('errored'));
-    wss.on('close', function() {
-        console.log('connection closed');
+async function main(number){
+    await chart.logMin.startFileLog()
+    await chart.logHour.startFileLog()
+    await chart.logDay.startFileLog()
+    await chart.logWeek.startFileLog()
+    wsServer()
+    httpServer()
+}
+
+function wsServer(){
+    wss.on('connection', function (ws,req) {
+        const location = url.parse(req.url, true);
+        ws.on('error', () => console.log('errored'));
+        wss.on('close', function() {
+            console.log('connection closed');
+        })
+        ws.on('message', function (message) {
+            if(parseFloat(message)){
+                //console.log('received data: %s', message)
+                message = parseFloat(message)
+                console.log('received data: %s', message)
+                chart.log(message,moment())
+            }
+            else{
+                console.log('received non JSON: %s', message)
+            }
+            wss.clients.forEach(function each(ws) {
+                msg.json = chart.getChartSecond()
+                msg.json_mins_60 = chart.getChartMinute()
+                msg.json_hours_60 = chart.getChartHour()
+                msg.json_days_60 = chart.getChartDay()
+                msg.json_weeks_60 = chart.getChartWeek()
+                ws.send(JSON.stringify(msg), (error)=>{
+                    if(error == undefined){
+                        return;
+                    }
+                    else{
+                        console.log("Async error:"+error);
+                        ws.terminate()
+                    }
+                })
+            });
+        })
+    })    
+}
+
+function httpServer(){
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
     })
-    ws.on('message', function (message) {
-        
-        if(parseFloat(message)){
-            //console.log('received data: %s', message)
-            chart.log(message,moment())
-        }
-        else{
-            console.log('received non JSON: %s', message)
-        }
-        wss.clients.forEach(function each(ws) {
-            msg.json = chart.getChartSecond()
-            msg.json_mins_60 = chart.getChartMinute()
-            msg.json_hours_60 = chart.getChartHour()
-            msg.json_days_60 = chart.getChartDay()
-            msg.json_weeks_60 = chart.getChartWeek()
-            ws.send(JSON.stringify(msg), (error)=>{
-                if(error == undefined){
-                    return;
-                }
-                else{
-                    console.log("Async error:"+error);
-                    ws.terminate()
-                }
-            })
-        });
+    app.get('/chartist_custom.css', function (req, res) {
+        res.sendFile(__dirname + '/chartist_custom.css');
     })
-})
+    
+    app.use(express.static(__dirname));
+    
+    server.listen(8080, function listening() {
+        console.log('Listening on %d', server.address().port);
+    });
+}
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-})
-app.get('/chartist_custom.css', function (req, res) {
-    res.sendFile(__dirname + '/chartist_custom.css');
-})
-
-app.use(express.static(__dirname));
-
-server.listen(8080, function listening() {
-    console.log('Listening on %d', server.address().port);
-});
+main()
---
+++ simulator.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws');
+const url = 'ws://127.0.0.1:8080/'
+var timer = null
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+function startSimulator(){
+    const ws = new WebSocket(url);
+    console.log("===============================\nTrying to connect to:",url)
+    ws.on('open', function open() {
+        console.log('connected');
+        timer = setInterval(function timeout() {
+            let random1 = getRandomInt(50)
+            let random2 = getRandomInt(50)
+            let number = random1 + random2
+            ws.send(number);
+            console.log("SENT:",number," --> ",url)
+        }, 1000);
+    });
+    
+    ws.on('close', function close() {
+      console.log('disconnected from websocket session',url);
+      clearInterval(timer);
+      timer = null
+    });
+    
+    ws.on('message', function incoming(data) {
+      //console.log(`Received Message`);
+    });
+    ws.on('error', function close(err) {
+        console.log('Websocket error for:',err);
+        clearInterval(timer);
+        timer = null
+      });
+  
+}
+
+setInterval(()=>{
+    if (timer == null){
+        startSimulator()
+    }
+},1000)
